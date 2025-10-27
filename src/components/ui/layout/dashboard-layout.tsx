f21eba7e0ac0f207372a1915bfeef31ace4a9146
J Mokgokong <jmokgokong@Js-MacBook-Pro.local>
2025-10-27 13:01:04 +0200

    use correct import path for Header component in dashboard-layout

--- src/components/ui/layout/dashboard-layout.tsx
+++ src/components/ui/layout/dashboard-layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './header';
+import Header from './Header';
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen ">
